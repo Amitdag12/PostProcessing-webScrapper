0f6e0048acd221cb34884d66412b637478c63870
Amitdag12 <gildag67@gmail.com>
2021-12-11 19:17:32 +0200

    v2

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 $tag='<div id="myModal" class="modal">
-
-<!-- Modal content -->
 <div class="modal-content">
   <span class="close">&times;</span>
   <p>Some text in the Modal..</p>
 </div>
-
 </div>';
-error_log($tag);
+//error_log($tag);
 error_log(json_encode(HtmlToJson($tag)));
 function GetWebsiteHtml($url)
 {
@@ -24,26 +21,62 @@ function HtmlToJson($htmlString)
     $htmlString=remove_html_comments($htmlString);//removes comments
     $htmlString=GetTextAfter("<", $htmlString);//gets tag text
     $tag=GetTextBefore(">", $htmlString);//gets tag text
+    $htmlString="<".$htmlString;
     //  $tag=HandleMissingSpace($tag);
     $tagAttributes=ExplodeTag($tag);//gets tag object attributes
     $tagContent ="";
-    $htmlString=DeleteTextBetween("<", ">", $htmlString);//deletes tag
     $tagObject=array();
     $tagObject["tagName"]=$tagAttributes[0];
     //$tagAttributes=array_shift($tagAttributes);
     $tagObject["tagAtrributes"]=$tagAttributes;
     $tagObject["children"]=array();
-    
-    
-    if (CheckIfAnyMore($htmlString)) {
-        $htmlString="<".GetTextAfter("<", $htmlString);
-        $result=HtmlToJson($htmlString);
-        $htmlString=$result[0];
-        $tagObject["children"][]=$result[1];
-    } else {
+    // error_log($htmlString);
+    $htmlString=DeleteTextBetween("<", ">", $htmlString);//deletes tag
+    if (!CheckIfAnyMore($htmlString)) {
+        $tagObject["content"]=GetTextBefore("<", $htmlString);
     }
-    $tagObject["content"]=GetTextBefore("<", $htmlString);
-    return array($htmlString,$tagObject);
+  
+
+    $htmlString=substr($htmlString, 0, FindFreeTag($htmlString));
+    
+    // error_log($htmlString);
+    //  return;
+    while (CheckIfAnyMore($htmlString)) {
+        $htmlString="<".GetTextAfter("<", $htmlString);
+        error_log("1:".$htmlString);
+        $result=HtmlToJson($htmlString);
+        $tagObject["children"][]=$result;
+        if (empty($result["children"])) {
+            //  error_log($htmlString);
+            error_log("2:".$result["tagAtrributes"][0]);
+            //    $htmlString=substr($htmlString, strpos($htmlString, "</".$result["tagAtrributes"][0].">")+strlen($result["tagAtrributes"][0])+3);
+            error_log("3:".$htmlString);
+            error_log("4:".json_encode($result));
+            if (trim($htmlString)=="") {
+                error_log("im out of here");
+                break;
+            }
+        }
+       
+        
+        // $htmlString=GetTextAfter("<", $htmlString);
+        //error_log($htmlString);
+        // $htmlString=substr($htmlString, 0, FindFreeTag($htmlString));
+        error_log("5:".$htmlString);
+        if (!array_key_exists("content", $tagObject)) {
+            $tagObject["content"]=GetTextBefore("<", $htmlString);
+        }
+        $htmlString=substr($htmlString, strpos($htmlString, "</".$result["tagAtrributes"][0].">")+strlen($result["tagAtrributes"][0])+3);
+        error_log("6:".$htmlString);
+    }
+    if ($tagObject["tagName"]=="p") {
+        error_log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    }
+    if (!array_key_exists("content", $tagObject)) {
+        $tagObject["content"]=GetTextBefore("<", $htmlString);
+    }
+    error_log("im out");
+    return $tagObject;
 }
 function GetTextBetween($start, $end, $text)
 {
@@ -75,7 +108,14 @@ function ExplodeTag($tag)
     $tagAttributes=array();
     $text="";
     $attributeObject=array();
-    $tagAttributes[]=GetTextBefore(' ', $tag);
+    if (strpos($tag, " ")!==false) {
+        $tagAttributes[]=GetTextBefore(' ', $tag);
+    } else {
+        $tagAttributes[]=$tag;
+        return $tagAttributes;
+    }
+    
+
     for ($i=strlen($tagAttributes[0]); $i <strlen($tag) ; $i++) {
         $currntChar=$tag[$i];
         if ($currntChar=='=') {
@@ -97,8 +137,12 @@ function ExplodeTag($tag)
 function CheckIfAnyMore($html)
 {
     $index=strpos($html, "<");
-    error_log("-------------".$index);
-    return $html[$index+1]!="/"&&$index !== false;
+    if ($index === false) {
+        return false;
+    }
+    echo $index."    ".$html[$index]."   ".$html[$index+1]."        ".json_encode($html[$index+1]!="/"&&$index !== false)."</br> ";
+    //  error_log("-------------".$index);
+    return $html[$index+1]!="/";
 }
 function remove_html_comments($content = '')
 {
@@ -107,11 +151,13 @@ function remove_html_comments($content = '')
 function FindFreeTag($html)
 {
     $count=0;
-    while (strpos($html, '<')!== false) {
+    while (CheckIfAnyMore($html)) {
         $indexStart=strpos($html, "<");
         $indexEnd=strpos($html, "</");
         $html[$indexStart]="A";
-        $html[$indexEnd]="BB";
+        $html[$indexEnd]="B";
+        $html[$indexEnd+1]="B";
+        //    error_log($html);
     }
     return strpos($html, "</");
 }
